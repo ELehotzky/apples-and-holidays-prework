f807514788e6458231ef46be65c8dec5a981a613
Erica Lehotzky <lehotzky.erica@gmail.com>
2018-07-14 18:15:36 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -65,7 +65,7 @@ def all_supplies_in_holidays(holiday_hash)
   holiday.each do |holiday, supplies|
     supplies = supplies.join(", ")
     holiday = holiday.to_s.split("_"). collect {|x| x.capitalize}.join(" ") << ":"
-    return "  #{holiday}: #{supplies}"
+    puts "  #{holiday}: #{supplies}"
   end
 end
 end
